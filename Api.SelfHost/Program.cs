7d415af30a0148ffa2347e0d81b2bc6130aaabc7
Niklas Olofsson <niklas.olofsson@ngenic.se>
2015-11-03 20:23:41 +0100

    Initial commit

---
+++ Api.SelfHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8080;
+            string uri = string.Format("http://localhost:{0}", port);
+            
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine(string.Format("Api in ready on: {0}", uri));
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Api/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Api
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "AllCustomers",
+                routeTemplate: "customers",
+                defaults: new { controller = "Customers", action = "Get" }
+                );
+        }
+    }
+}
